6d6a57f64fdeaf8ef0eb4bf9cb16c32191e57e96
David Mihal <david@dmihal.com>
2021-08-10 18:09:36 +0100

    Minor changes

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,21 +1,23 @@
 import { Address, BigInt, ethereum } from "@graphprotocol/graph-ts"
-import { Fees, DateToBlock } from "../generated/schema"
+import { Fee, DateToBlock } from "../generated/schema"
 import { ArbGasInfo } from "../generated/Fees/ArbGasInfo"
 import { ArbStatistics } from "../generated/Fees/ArbStatistics"
 
 let EIGHTEEN_DECIMALS = BigInt.fromI32(10).pow(18).toBigDecimal()
-let EIGHT_DECIMALS = BigInt.fromI32(10).pow(8).toBigDecimal()
+
+let ARB_STATS_ADDRESS = Address.fromString('0x000000000000000000000000000000000000006F')
+let ARB_GAS_ADDRESS = Address.fromString('0x000000000000000000000000000000000000006C')
 
 export function handleBlock(block: ethereum.Block): void {
-  let entity = Fees.load('1')
+  let entity = Fee.load('1')
   if (!entity) {
-    entity = new Fees('1')
-    entity.feesETH = BigInt.fromI32(0).toBigDecimal()
+    entity = new Fee('1')
+    entity.totalFeesETH = BigInt.fromI32(0).toBigDecimal()
     entity.totalArbGas = BigInt.fromI32(0)
   }
 
-  let stats = ArbStatistics.bind(Address.fromString('0x000000000000000000000000000000000000006F'))
-  let gasInfo = ArbGasInfo.bind(Address.fromString('0x000000000000000000000000000000000000006C'))
+  let stats = ArbStatistics.bind(ARB_STATS_ADDRESS)
+  let gasInfo = ArbGasInfo.bind(ARB_GAS_ADDRESS)
 
   let weiPerArbGas = gasInfo.getPricesInWei().value5
   let totalArbGas = stats.getStats().value3
@@ -24,7 +26,7 @@ export function handleBlock(block: ethereum.Block): void {
   let ethFees = newGas.times(weiPerArbGas).divDecimal(EIGHTEEN_DECIMALS)
 
   entity.totalArbGas = totalArbGas
-  entity.feesETH += ethFees
+  entity.totalFeesETH += ethFees
   entity.save()
 
   let timestampRoundedDown = block.timestamp.toI32() / 86400 * 86400
